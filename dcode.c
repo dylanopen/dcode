2498516639c55d666262c816cdb32b7f35391bdd
The Open Project <opendylan@proton.me>
2023-02-11 22:42:25 +0000

    editor 0.0.3

--- dcode.c
+++ dcode.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -35,12 +36,18 @@ enum editorKey {
 
 /*** data ***/
 
-
+typedef struct erow
+{
+	int size;
+	char *chars;
+} erow;
 
 struct editorConfig {
 	int cx, cy;
 	int screenrows;
 	int screencols;
+	int numrows;
+	erow row;
 	struct termios orig_termios;
 };
 
@@ -178,6 +185,15 @@ int getWindowSize(int *rows, int *cols) {
 	}
 }
 
+void editorOpen() {
+	char *line = "Hello, world!";
+	ssize_t linelen = 13;
+	E.row.size = linelen;
+	E.row.chars = malloc(linelen + 1);
+	memcpy(E.row.chars, line, linelen);
+	E.row.chars[linelen] = '\0';
+	E.numrows = 1;
+}
 
 /*** append buffer ***/
 
@@ -213,21 +229,27 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
 	int y;
 	for (y = 0; y < E.screenrows; y++) {
-		if (y == 0) {
-			char welcome[80];
-			int welcomelen = snprintf(welcome, sizeof(welcome),
-				"dcode %s", VERSION);
-			if (welcomelen > E.screencols) welcomelen = E.screencols;
-			int padding = (E.screencols - welcomelen);
-			if (padding) {
+		if (y >= E.numrows) {
+			if (y == 0) {
+				char welcome[80];
+				int welcomelen = snprintf(welcome, sizeof(welcome),
+					"dcode %s", VERSION);
+				if (welcomelen > E.screencols) welcomelen = E.screencols;
+				int padding = (E.screencols - welcomelen);
+				if (padding) {
+					abAppend(ab, "~", 1);
+					padding--;
+				}
+				while (padding--) abAppend(ab, " ", 1);
+				abAppend(ab, welcome, welcomelen);
+			} else {
 				abAppend(ab, "~", 1);
-				padding--;
 			}
-			while (padding--) abAppend(ab, " ", 1);
-			abAppend(ab, welcome, welcomelen);
-		} else {
-			abAppend(ab, "~", 1);
-		}
+	} else {
+		int len = E.row.size;
+		if (len > E.screencols) len = E.screencols;
+		abAppend(ab, E.row.chars, len);
+	}
 
 
 		abAppend(ab, "\x1b[K", 3);
@@ -341,7 +363,7 @@ void editorProcessKeypress() {
 void initEditor() {
 	E.cx = 0;
 	E.cy = 0;
-
+	E.numrows = 0;
 
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
@@ -350,7 +372,7 @@ void initEditor() {
 int main() {
 	enableRawMode();
 	initEditor();
-
+	editorOpen();
 
 	while (1) {
 		editorRefreshScreen();
